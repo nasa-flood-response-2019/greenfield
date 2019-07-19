3fbe7517d5deefd5be90a9fad2da1f371f9df3ad
haleyysanchez <haleymsanchez910@gmail.com>
2019-07-19 00:15:18 -0500

    Merge branch 'master' of https://github.com/nasa-flood-response-2019/greenfield
    # Conflicts:
    #	src/components/Map.vue

--- src/App.vue
+++ src/App.vue
@@ -15,15 +15,14 @@
     import Map from './components/Map.vue';
     import Header from './components/Header.vue';
     import Navigation from './components/Navigation.vue';
-    import Test from './components/Test.vue';
+
 
     export default {
         name: "App1",
         components: {
             Map,
             Navigation,
-            Header,
-            Test
+            Header
         }
     }
 </script>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -24,46 +24,165 @@
                         </v-btn>
                     </v-list-tile-action>
                 </v-list-tile>
+                <v-divider></v-divider>
+                <v-list-group
+                        :value="false"
+                >
+                    <!--                        prepend-icon="map"-->
+                    <template v-slot:activator>
+                        <v-list-tile>
+                            <v-list-tile-avatar>
+                                <v-btn
+                                        icon
+                                        @click.stop="mini = !mini"
+                                >
+                                    <v-icon>{{items[1].icon}}</v-icon>
+                                </v-btn>
+                            </v-list-tile-avatar>
+                            <v-list-tile-content>
+                                <v-list-tile-title>Basemaps</v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </template>
+                    <v-list-group
+                            no-action
+                            sub-group
+                            :value="false"
+                    >
+                        <template v-slot:activator>
+                            <v-list-tile>
+                                <v-list-tile-title>Basemap 1</v-list-tile-title>
+                            </v-list-tile>
+                        </template>
+                        <v-list-tile
+                                v-for="(basemap1, i) in basemap1"
+                                :key="i"
+                                @click=""
+                        >
+                            <v-list-tile-title v-text="basemap1[0]"></v-list-tile-title>
+                            <v-list-tile-action>
+                                <v-icon v-text="basemap1[1]"></v-icon>
+                            </v-list-tile-action>
+                        </v-list-tile>
+                    </v-list-group>
+                </v-list-group>
+                <!--                    MENU ITEM 2-->
+                <v-list-group
+                        prepend-icon="layers"
+                        :value="false"
+                >
+                    <template v-slot:activator>
+                        <v-list-tile>
+                            <v-list-tile-title>Data Layers</v-list-tile-title>
+                        </v-list-tile>
+                    </template>
+                    <v-list-group
+                            no-action
+                            sub-group
+                            :value="false"
+                    >
+                        <template v-slot:activator>
+                            <v-list-tile>
+                                <v-list-tile-title>DataLayer 1</v-list-tile-title>
+                            </v-list-tile>
+                        </template>
+                        <v-list-tile
+                                v-for="(data1, i) in data1"
+                                :key="i"
+                                @click=""
+                        >
+                            <v-list-tile-title v-text="data1[0]"></v-list-tile-title>
+                            <v-list-tile-action>
+                                <v-icon v-text="data1[1]"></v-icon>
+                            </v-list-tile-action>
+                        </v-list-tile>
+                    </v-list-group>
+                </v-list-group>
+                <!--            <v-list-tile-->
+                <!--                    v-for="item in items"-->
+                <!--                    :key="item.title"-->
+                <!--                    @click=""-->
+                <!--            >-->
+                <!--                <v-list-tile-action>-->
+                <!--                    <v-icon>{{ item.icon }}</v-icon>-->
+                <!--                </v-list-tile-action>-->
+                <!--                <v-list-tile-content>-->
+                <!--                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>-->
+                <!--                </v-list-tile-content>-->
+                <!--            </v-list-tile>-->
+                <!--            <v-list-tile-->
+                <!--                    v-for="item in items"-->
+                <!--                    :key="item.title"-->
+                <!--                    @click=""-->
+                <!--            >-->
+                <!--                <v-list-tile-action>-->
+                <!--                    <v-icon>{{ item.icon }}</v-icon>-->
+                <!--                </v-list-tile-action>-->
+                <!--                <v-list-tile-title>-->
+                <!--                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>-->
+                <!--                    <v-list-group-->
+                <!--                            no-action-->
+                <!--                            sub-group-->
+                <!--                            value="true"-->
+                <!--                            v-if="drop">-->
+                <!--                        <v-list-tile-content>-->
+                <!--                                <v-list-tile>-->
+                <!--                                    <v-list-tile-title>Basemaps</v-list-tile-title>-->
+                <!--                                </v-list-tile>-->
+                <!--                    <v-list-tile-->
+                <!--                            v-for="(base, i) in bases"-->
+                <!--                            :key="i"-->
+                <!--                            @click=""-->
+                <!--                    >-->
+                <!--                        <v-list-tile-title v-text="base[0]"></v-list-tile-title>-->
+                <!--                        <v-list-tile-action>-->
+                <!--                            <v-icon v-text="base[1]"></v-icon>-->
+                <!--                        </v-list-tile-action>-->
+                <!--                    </v-list-tile>-->
+                <!--                        </v-list-tile-content>-->
+                <!--                    </v-list-group>-->
+                <!--                </v-list-tile-title>-->
+                <!--            </v-list-tile>-->
             </v-list>
         </v-toolbar>
-        <v-list class="pt-0" dense>
-            <v-divider></v-divider>
-            <v-list-tile
-                    v-for="item in items"
-                    :key="item.title"
-                    @click=""
-            >
-                <v-list-tile-action>
-                    <v-icon>{{ item.icon }}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
-        </v-list>
     </v-navigation-drawer>
 </template>
 <script>
     //import Header from './components/Header.vue';
     export default {
         name: "Navigation",
-        data () {
+        props:
+            {
+                width: 400
+            },
+        data(){
             return {
                 drawer: true,
                 items: [
-                    { title: 'Home', icon: 'home' },
-                    { title: 'BaseMaps', icon: 'map' },
-                    { title: 'Data Layers', icon: 'layers' },
-                    { title: 'Sliders', icon: 'opacity' },
-                    { title: '3D scene', icon: '3d_rotation' },
-                    { title: 'About', icon: 'info' }
+                    {title: 'Home', icon: 'home', drop: false, check: false},
+                    {title: 'Basemaps', icon: 'map', drop: true, check: true},
+                    {title: 'Data Layers', icon: 'layers', drop: false, check: false},
+                    //embed sliders to data layers
+                    //have sliders pop up on screen when new data layer added
+                    // { title: 'Opacity', icon: 'opacity' },
+                    {title: '3D scene', icon: '3d_rotation', drop: false, check: false},
+                    {title: 'About', icon: 'info', drop: false, check: false}
+                ],
+                basemap1: [
+                    ['Feature 1', 'map'],
+                    ['Feature 2', 'home']
+                ],
+                data1: [
+                    ['Data 1', 'map'],
+                    ['Data 2', 'home']
                 ],
                 mini: true,
-                right: null
+                right: null,
+                bases: [
+                    ['Default', 'map'],
+                    ['Streets', 'map']
+                ]
             }
-        },
-        props: {
-            width: 400
         }
     }
 </script>
