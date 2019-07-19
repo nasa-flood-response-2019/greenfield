a062417e33dcfaccfd49b1f73639abd04ea5826a
haleyysanchez <haleymsanchez910@gmail.com>
2019-07-18 23:46:21 -0500

    Merge branch 'master' of https://github.com/nasa-flood-response-2019/greenfield
    # Conflicts:
    #	src/components/Map.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
             <Navigation/>
             <Header/>
             <Map/>
-
+<!--                <Test/>-->
         </v-container>
         </v-content>
     </v-app>
@@ -15,13 +15,15 @@
     import Map from './components/Map.vue';
     import Header from './components/Header.vue';
     import Navigation from './components/Navigation.vue';
+    import Test from './components/Test.vue';
 
     export default {
         name: "App1",
         components: {
             Map,
             Navigation,
-            Header
+            Header,
+            Test
         }
     }
 </script>
