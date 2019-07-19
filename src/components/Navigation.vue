8c08f38f286425c1c102c3e68cd2e8507b6636aa
reachanavi <reachanavi@gmail.com>
2019-07-18 23:15:49 -0500

    changed zoom position

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -24,23 +24,31 @@
                         </v-btn>
                     </v-list-tile-action>
                 </v-list-tile>
-            </v-list>
-        </v-toolbar>
-        <v-list class="pt-0" dense>
+
+
+
             <v-divider></v-divider>
-            <v-list-tile
-                    v-for="item in items"
-                    :key="item.title"
-                    @click=""
-            >
-                <v-list-tile-action>
-                    <v-icon>{{ item.icon }}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
+
+                <v-list-tile avatar>
+                    <v-list-tile-avatar>
+
+                    </v-list-tile-avatar>
+
+                </v-list-tile>
+<!--            <v-list-tile-->
+<!--                    v-for="item in items"-->
+<!--                    :key="item.title"-->
+<!--                    @click=""-->
+<!--            >-->
+<!--                <v-list-tile-action>-->
+<!--                    <v-icon>{{ item.icon }}</v-icon>-->
+<!--                </v-list-tile-action>-->
+<!--                <v-list-tile-content>-->
+<!--                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>-->
+<!--                </v-list-tile-content>-->
+<!--            </v-list-tile>-->
         </v-list>
+        </v-toolbar>
     </v-navigation-drawer>
 </template>
 <script>
