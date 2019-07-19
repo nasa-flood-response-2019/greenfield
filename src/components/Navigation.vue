4fa21fcb3aa13205fd54b63f2b714e3b7816b776
haleyysanchez <haleymsanchez910@gmail.com>
2019-07-19 11:32:34 -0500

    Merge branch 'master' of https://github.com/nasa-flood-response-2019/greenfield
    # Conflicts:
    #	src/components/Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -99,17 +99,26 @@
                             </v-list-tile-content>
                         </v-list-tile>
                     </template>
-                    <v-list-tile
-                            v-for="(data1, i) in data1"
-                            :key="i"
-                            @click=""
-                    >
-                        <v-list-tile-title v-text="data1[0]"></v-list-tile-title>
+                    <v-list-tile @click="">
                         <v-list-tile-action>
-                            <v-icon v-text="data1[1]"></v-icon>
+                            <v-checkbox v-model="video"></v-checkbox>
                         </v-list-tile-action>
 
+                        <v-list-tile-content @click="video = !video">
+                            <v-list-tile-title>Video sounds</v-list-tile-title>
+                        </v-list-tile-content>
                     </v-list-tile>
+                    <!--                    <v-list-tile-->
+<!--                            v-for="(data1, i) in data1"-->
+<!--                            :key="i"-->
+<!--                            @click=""-->
+<!--                    >-->
+<!--                        <v-list-tile-title v-text="data1[0]"></v-list-tile-title>-->
+<!--                        <v-list-tile-action>-->
+<!--                            <v-icon v-text="data1[1]"></v-icon>-->
+<!--                        </v-list-tile-action>-->
+
+<!--                    </v-list-tile>-->
                 </v-list-group>
                 <v-list-tile>
                     <v-list-tile-avatar>
@@ -153,14 +162,14 @@
             return {
                 drawer: true,
                 items: [
-                    {title: 'Home', icon: 'home', drop: false, check: false},
-                    {title: 'BaseMaps', icon: 'map', drop: true, check: true},
-                    {title: 'Data Layers', icon: 'layers', drop: false, check: false},
+                    {title: 'Home', icon: 'home'},
+                    {title: 'BaseMaps', icon: 'map'},
+                    {title: 'Data Layers', icon: 'layers'},
                     //embed sliders to data layers
                     //have sliders pop up on screen when new data layer added
                     // { title: 'Opacity', icon: 'opacity' },
-                    {title: '3D scene', icon: '3d_rotation', drop: false, check: false},
-                    {title: 'About', icon: 'info', drop: false, check: false}
+                    {title: '3D scene', icon: '3d_rotation'},
+                    {title: 'About', icon: 'info'}
                 ],
                 basemap1: [
                     ['Default', 'map'],
