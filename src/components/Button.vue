2b3a35fa980396d69a95678ac7a7085bb025e9c1
haleyysanchez <haleymsanchez910@gmail.com>
2019-07-24 11:18:40 -0500

    j

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,14 +11,14 @@
             openScene: function() {
                 this.$eventHub.$emit('openScene');
             },
-            onClick: function() {
-                console.log("started onClick() function");
-                // the three latter numbers are the x,y,z coordinates, respectively
-                this.$emit('clicked', '34568765', '-1222', '4565');
-                console.log("emitted clicked");
-                this.$eventHub.$emit('openBox');
-                console.log("emitted openBox");
-            }
+            // onClick: function() {
+            //     console.log("started onClick() function");
+            //     // the three latter numbers are the x,y,z coordinates, respectively
+            //     this.$emit('clicked', '34568765', '-1222', '4565');
+            //     console.log("emitted clicked");
+            //     this.$eventHub.$emit('openBox');
+            //     console.log("emitted openBox");
+            // }
         }
     }
 </script>
