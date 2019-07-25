8b9fa21cd55e2f8f246cb3ec50c0b0ebcef9745c
Caroline Choi <38522055+carolinechoi@users.noreply.github.com>
2019-07-25 13:09:21 -0500

    Update Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-btn id="button" class="mx-2" fab dark color="indigo" @click="openScene">
+    <v-btn id="button" class="mx-2" fab dark color="indigo" @click="onClick">
         <v-icon dark>3d_rotation</v-icon>
     </v-btn>
 </template>
@@ -8,17 +8,17 @@
     export default {
         name: "Button",
         methods: {
-            openScene: function() {
-                this.$eventHub.$emit('openScene');
-            },
             // onClick: function() {
-            //     console.log("started onClick() function");
-            //     // the three latter numbers are the x,y,z coordinates, respectively
-            //     this.$emit('clicked', '34568765', '-1222', '4565');
-            //     console.log("emitted clicked");
-            //     this.$eventHub.$emit('openBox');
-            //     console.log("emitted openBox");
-            // }
+            //     this.$eventHub.$emit('openScene');
+            // },
+            onClick: function() {
+                console.log("started onClick() function");
+                // the three latter numbers are the x,y,z coordinates, respectively
+                // this.$emit('clicked', '34568765', '-1222', '4565');
+                // console.log("emitted clicked");
+                this.$eventHub.$emit('openBox');
+                console.log("emitted openBox");
+            }
         }
     }
 </script>
@@ -26,7 +26,7 @@
     #button{
         z-index: 1021;
         top: 0px;
-        right: 9px;
+        right: 1.29%;
         position: absolute;
     }
 
